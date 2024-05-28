a7424ec4c0d55101995f65cbff8376eba74ca548
xie-qingcheng <scarlettv.0208@gmail.com>
2024-05-27 18:22:30 -0600

    javascript

---
+++ index.js
@@ -0,0 +1,3 @@
+const container = document.querySelector("#container") //acceder a un selector html para guardarlo en una variable
+//container.innerHTML = "" (para borrar el contenido de la variable)
+//container.style.backgroundColor='red' (cambiar el css)
