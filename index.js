e7355b9747ad3b7fb0c369c940a84b6e1b0f32d8
xie-qingcheng <scarlettv.0208@gmail.com>
2024-05-29 19:37:16 -0500

    conectar a la API
    Se conecta a la API y se agrega la informacion de personajes (character)

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const container = document.querySelector("#container") //acceder a un selector html para guardarlo en una variable
 //container.innerHTML = "" (para borrar el contenido de la variable)
 //container.style.backgroundColor='red' (cambiar el css)
+
+const url = "https://rickandmortyapi.com/api"
+
+fetch(url + "/character")//consulta informacion
+.then(response => response.json()) //se guarda en la variable response y lo cambia a un tipo de datos json
+.then(json => {
+    const characters = json.results
+    characters.forEach(character => {
+        container.innerHTML += character.name
+    })
+})
