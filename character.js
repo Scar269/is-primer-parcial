55869f4bc905b3c825cba7720418d697b0067141
xie-qingcheng <scarlettv.0208@gmail.com>
2024-06-06 13:39:52 -0500

    individual card css

--- character.js
+++ character.js
@@ -10,12 +10,12 @@ fetch(urlAPI + "/character/" + id)//consulta informacion
             <div id="character">
                 <img src="${character.image}" alt="">
                 <div class="detail">
-                    <h1>${character.name}</h1>
+                    <h1 class="yellow">${character.name}</h1>
                     <h2>${character.status} - ${character.species}</h2>
-                    <p>Last known location:</p>
-                    <p>${character.origin.name}</p>
-                    <p>First seen in:</p>
-                    <p>${character.location.name}</p>
+                    <p class="gray">Last known location:</p>
+                    <p class="yellow">${character.origin.name}</p>
+                    <p class="gray">First seen in:</p>
+                    <p class="yellow">${character.location.name}</p>
                 </div>
             </div>`
         characterContainer.innerHTML += characterTemplate
